be2554cc7f7c831a5ab26b2ce7785af662b48e50
Soeky <seymeny527@gmail.com>
2025-05-20 08:00:06 +0200

    added files

---
+++ neetcode/arrays/sols.cpp
@@ -0,0 +1,64 @@
+#define ll long long
+
+
+void longestConsecutiveSequence(){
+
+}
+
+
+void validSudoku(){
+
+}
+
+
+void productOfArrayExceptSelf(){
+
+}
+
+
+void encodeAndDecodeStrings(){
+
+}
+
+
+void topKFrequentElements(){
+
+}
+
+
+void groupAnagrams(){
+
+}
+
+
+void twoSum(){
+
+}
+
+
+void validAnagram(){
+
+}
+
+
+void containsDuplicate(){
+
+}
+
+void solve(){
+    containsDuplicate();
+    validAnagram();
+    twoSum();
+    groupAnagrams();
+    topKFrequentElements();
+    encodeAndDecodeStrings();
+    productOfArrayExceptSelf();
+    validSudoku();
+    longestConsecutiveSequence();
+}
+
+int main(){
+    return 0;
+}
+
+
---
+++ neetcode/sols.cpp
@@ -0,0 +1,10 @@
+#define ll long long
+
+void solve(){
+}
+
+int main(){
+    return 0;
+}
+
+
